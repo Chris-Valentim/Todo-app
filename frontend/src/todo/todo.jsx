d610183ec00588a784e4c520744c20812add1ec4
Christian Valentim <christianvalentim99@gmail.com>
2024-06-07 19:18:23 -0300

    Projeto todoapp

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import PageHeader from '../template/pageHeader'
+import TodoForm from "./todoForm"
+import TodoList from "./todoList"
+
+const Todo = () => {
+  return (
+    <div>
+      <PageHeader name='Tarefas' small='Cadastro' />
+      <TodoForm />
+      <TodoList />
+    </div>
+  )
+}
+
+export default Todo
